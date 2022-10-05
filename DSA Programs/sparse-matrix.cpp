bb458fa4b74e3312efb7307c2b556865b4d1356f
Reubzz <reugeorac@gmail.com>
2022-10-06 02:23:57 +0530

    Minor bug fixes

--- DSA Programs/sparse-matrix.cpp
+++ DSA Programs/sparse-matrix.cpp
@@ -1,61 +1,46 @@
-#include<iostream.h>
-#include<conio.h>
+#include <iostream>
+#include <conio.h>
 
+using namespace std;
 
-void main()
-{
-int i,j,n,m;
-int mat[10][10];
-int dia[10];
- 
-cout<<"enter the number of rows andd coloumns u want in the matrix "<<endl;
-cin>>n>>m;
+int main() {
+    int i, j, n, m, mat[10][10], dia[10];
 
-cout<<"Enter the diagnol elements value"<<endl;
-for(i=0;i<n;i++)
-{
-cin>>dia[i];
-}
+    cout << "enter the number of rows andd coloumns u want in the matrix " << endl;
+    cin >> n >> m;
 
-
-cout<<"displaying the diagnol elements"<<endl;
-for(i=0;i<n;i++)
-{
-cout<<dia[i]<<",";
-}
-
-cout<<endl;
-
-for(i=0;i<n;i++)
-{
-    for(j=0;j<n;j++)
-    {
-        cin>>mat[i][j];
+    cout << "Enter the diagnol elements value" << endl;
+    for (i = 0; i < n; i++) {
+        cin >> dia[i];
     }
-}
-cout<<endl;
 
-
-int trans=0;
-cout<<"Displaying the sparse of a matrix:"<<endl;
-for(i=0;i<m;i++)
-{
-    for(j=0;j<n;j++)
-{   
-    if(i==j)
-    cout<<dia[i];
-    else
-    {
-        cout<<mat[i][j];
+    cout << "displaying the diagnol elements" << endl;
+    for (i = 0; i < n; i++) {
+        cout << dia[i] << ",";
     }
-    cout<<"  ";
+
+    cout << endl;
+
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            cin >> mat[i][j];
+        }
+    }
+    cout << endl;
+
+    int trans = 0;
+    cout << "Displaying the sparse of a matrix:" << endl;
+    for (i = 0; i < m; i++) {
+        for (j = 0; j < n; j++) {
+            if (i == j)
+                cout << dia[i];
+            else {
+                cout << mat[i][j];
+            }
+            cout << "  ";
+        }
+        cout << "\n";
+    }
+    getch();
+    return 0;
 }
-cout<<"\n";
-}
-getch();
-}
-
-
-
-
-
