d850ab1eaeaa51428e66b4b6bebfebb017af7974
Reubz <reugeorac@gmail.com>
2022-10-07 13:03:47 +0530

    minor changes

--- DSA Programs/doubly-linked-list.cpp
+++ DSA Programs/doubly-linked-list.cpp
@@ -1,25 +1,22 @@
-#include<iostream>
-#include<conio.h>
+#include <iostream>
+#include <conio.h>
 
 using namespace std;
 
-
-struct dnode
-{
+struct dnode{
   int data;
   struct dnode *rlink;
   struct dnode *llink;
 };
-void
-main ()
-{
+
+int main(){
   struct dnode *head, *ptr, *newnode;
   int ele, rep;
   int sa = 1;
   // clrscr ();
 
-
-  cout << endl << "enter the first value";
+  cout << endl
+       << "enter the first value";
   cin >> ele;
 
   newnode = new dnode;
@@ -30,142 +27,109 @@ main ()
   head = newnode;
   ptr = head;
 
-  do
-    {
-      cout << "enter the value" << endl;
-      cin >> ele;
+  do{
+    cout << "enter the value" << endl;
+    cin >> ele;
 
+    newnode = new dnode;
+    newnode->data = ele;
+    newnode->rlink = NULL;
+    newnode->llink = NULL;
 
-      newnode = new dnode;
-      newnode->data = ele;
-      newnode->rlink = NULL;
-      newnode->llink = NULL;
+    ptr->rlink = newnode;
+    newnode->llink = ptr;
+    ptr = ptr->rlink;
 
-      ptr->rlink = newnode;
-      newnode->llink = ptr;
-      ptr = ptr->rlink;
+    cout << "any more data?(1=yes/0=no)" << endl;
+    cin >> rep;
 
-      cout << "any more data?(1=yes/0=no)" << endl;
-      cin >> rep;
+  } while (rep);
 
-    }
-  while (rep);
   cout << "The doubly linked list is :";
   ptr = head;
-  while (ptr != NULL)
-    {
-      cout << ptr->data << " ";
-      ptr = ptr->rlink;
-    }
+
+  while (ptr != NULL){
+    cout << ptr->data << " ";
+    ptr = ptr->rlink;
+  }
 
   cout << endl;
-
 
   ptr = newnode;
   cout << "The reverse linked list is :";
-  while (ptr != NULL)
-    {
-      cout << ptr->data << " ";
-      ptr = ptr->llink;
-
-    }
-
-
-
-
+  
+  while (ptr != NULL){
+    cout << ptr->data << " ";
+    ptr = ptr->llink;
+  }
 
   int val;
-  while (sa)
-    {
-      cout << "enter the value" << endl;
-      cin >> ele;
-      newnode = new dnode;
-      newnode->data = ele;
-      newnode->rlink = NULL;
-      newnode->llink = NULL;
-      cout << "After which value you want to insert" << endl;
-      cin >> val;
-      ptr = head;
+  while (sa){
+    cout << "enter the value" << endl;
+    cin >> ele;
+    newnode = new dnode;
+    newnode->data = ele;
+    newnode->rlink = NULL;
+    newnode->llink = NULL;
+    cout << "After which value you want to insert" << endl;
+    cin >> val;
+    ptr = head;
 
-      while (ptr)
-	{
-	  if (ptr->data != val)
-	    ptr = ptr->rlink;
-	  else
-	    {
-	      newnode->rlink = ptr->rlink;
-	      newnode->llink = ptr;
-	      ptr->rlink = newnode;
-	      (newnode->rlink)->llink = newnode;
-	      ptr = ptr->rlink;
-	    }
-	}
-      cout << "The updated doubly linked list is :";
-      ptr = head;
-      while (ptr != NULL)
-	{
-	  cout << ptr->data << " ";
-	  ptr = ptr->rlink;
-
-	}
-
-      cout << endl;
-      cout << "whether you want to insert more data (1=yes/0=n0)" << endl;
-      cin >> sa;
+    while (ptr){
+      if (ptr->data != val)
+        ptr = ptr->rlink;
+      else {
+        newnode->rlink = ptr->rlink;
+        newnode->llink = ptr;
+        ptr->rlink = newnode;
+        (newnode->rlink)->llink = newnode;
+        ptr = ptr->rlink;
+      }
+    }
+    cout << "The updated doubly linked list is :";
+    ptr = head;
+    while (ptr != NULL) {
+      cout << ptr->data << " ";
+      ptr = ptr->rlink;
     }
 
-  cout << endl;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    cout << endl;
+    cout << "whether you want to insert more data (1=yes/0=n0)" << endl;
+    cin >> sa;
+  }
 
   cout << endl;
 
+  cout << endl;
 
-//----------------------------------
+  //----------------------------------
   int fu = 1;
-  while (fu)
-    {
-      cout << "enter the data which you want to delete" << endl;
-      cin >> val;
+  while (fu){
+    cout << "enter the data which you want to delete" << endl;
+    cin >> val;
 
-      ptr = head;
-      while (ptr)
-	{
-	  if (ptr->data != val)
-	    {
-	      ptr = ptr->rlink;
-	    }
-	  else
-	    {
-	      (ptr->llink)->rlink = ptr->rlink;
-	      ptr = ptr->rlink;
-	    }
-	}
-      cout << "the linked list after deletion:" << endl;
-      ptr = head;
-      while (ptr != NULL)
-	{
-	  cout << ptr->data << " ";
-	  ptr = ptr->rlink;
-	}
-      cout << endl;
-      cout << "whether you want to delete more data (1=yes/0=n0)" << endl;
-      cin >> fu;
+    ptr = head;
+    while (ptr){
+      if (ptr->data != val){
+        ptr = ptr->rlink;
+      }
+      else{
+        (ptr->llink)->rlink = ptr->rlink;
+        ptr = ptr->rlink;
+      }
     }
+    cout << "the linked list after deletion:" << endl;
+    ptr = head;
+    
+    while (ptr != NULL){
+      cout << ptr->data << " ";
+      ptr = ptr->rlink;
+    }
+    cout << endl;
+    cout << "whether you want to delete more data (1=yes/0=n0)" << endl;
+    cin >> fu;
+  }
 
-  getch ();
+  getch();
+  return 0;
 }
