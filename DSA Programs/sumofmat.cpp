db225636dbf4ae1f8d230069aa2c3711d16bf39f
Reuben J George <74487020+Reubzz@users.noreply.github.com>
2023-10-03 23:36:06 +0530

    Update and rename
    updated app to better fluent code understanding

--- DSA Programs/sumofmat.cpp
+++ DSA Programs/sumofmat.cpp
@@ -1,37 +1,40 @@
 #include <iostream>
 
 
-using namespace std;
+using namespace std; // remove in turbo c
 
 int main()
 {
     //   clrscr (); // use only in turbo C.
-    int i, j, n, x, m;
-    int name;
+    int i, j, n, m;
     int mat[10][10];
     int A[10][10];
     int B[10][10];
+
+    // MATRIX DATA
     cout << "Enter the Number of rows" << endl;
     cin >> n;
-    cout << "Enter the Number Of coloumns" << endl;
+    cout << "Enter the Number Of columns" << endl;
     cin >> m;
 
-    cout << "Enter the matrix value" << endl;
 
+    // FIRST MATRIX
+     cout << "Enter the second matrix" << endl;
     for (i = 0; i < n; i++){
         for (j = 0; j < m; j++){
             cout << "Enter element for a" << i + 1 << j + 1 << ":" << endl;
             cin >> A[i][j];
         }
     }
-    cout << "Entered matrix" << endl;
+    cout << "Entered matrix A" << endl;
     for (i = 0; i < n; i++){
         for (j = 0; j < m; j++){
             cout << A[i][j] << "  ";
         }
         cout << endl;
     }
-    
+
+    // SECOND MATRIX 
     cout << "Enter the second matrix" << endl;
     for (i = 0; i < n; i++){
         for (j = 0; j < m; j++){
@@ -39,7 +42,7 @@ int main()
             cin >> B[i][j];
         }
     }
-    cout << "Entered matrix" << endl;
+    cout << "Entered matrix B" << endl;
     for (i = 0; i < n; i++){
         for (j = 0; j < m; j++){
             cout << B[i][j] << "  ";
@@ -52,13 +55,13 @@ int main()
         }
     }
 
-    cout << "sum of the given  two matrix" << endl;
+    cout << "sum of the given two matrices" << endl;
     for (i = 0; i < n; i++){
         for (j = 0; j < m; j++){
             cout << mat[i][j];
             cout << "  ";
         }
         cout << endl;
-        cout << "\n";
     }
+    getch(); // Add in Turbo C
 }
